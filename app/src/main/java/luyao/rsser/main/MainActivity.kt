4dde85ec733467632c874938b40cf936e84583c5
luyao <sunluyao1993x@gmail.com>
2020-02-24 16:59:59 +0800

    init okhttp

--- app/src/main/java/luyao/rsser/main/MainActivity.kt
+++ app/src/main/java/luyao/rsser/main/MainActivity.kt
@@ -1,8 +1,15 @@
 package luyao.rsser.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import luyao.mvvm.core.base.BaseVMActivity
 import luyao.rsser.R
+import luyao.rsser.model.OkHttpClient
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 
 class MainActivity : BaseVMActivity<MainViewModel>(false) {
@@ -16,6 +23,18 @@ class MainActivity : BaseVMActivity<MainViewModel>(false) {
     }
 
     override fun initData() {
+        val okHttp = OkHttpClient()
+        val call = okHttp.client.newCall(Request.Builder().url("https://luyao.tech/feed.xml").build())
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("xxx", e.message ?:"")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val result = response.body?.string()
+                Log.e("xxx",result?:"null")
+            }
+        })
     }
 
     override fun startObserve() {
---
+++ app/src/main/java/luyao/rsser/model/OkHttpClient.kt
@@ -0,0 +1,10 @@
+package luyao.rsser.model
+
+import luyao.mvvm.core.net.BaseOkHttpClient
+
+/**
+ * Created by luyao
+ * on 2020/2/24 16:45
+ */
+class OkHttpClient : BaseOkHttpClient() {
+}
